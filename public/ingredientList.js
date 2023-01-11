d1aa8a86a454495f0e07fca1f68a2eea6fddb069
LopezJRR <114459754+LopezJRR@users.noreply.github.com>
2023-01-10 21:44:24 -0800

    updated js files
    Modified ingredients.js and cart.js in accordance to the activity guide.

--- public/ingredientList.js
+++ public/ingredientList.js
@@ -1,18 +1,21 @@
+const ingredientList = {
+
 // The array of ingredients the user can add to a sandwich
 //  This will be updated after we fetch.
-let ingredients = [];
+    ingredients: [],
 
 // Updates the DOM to display a list of ingredients
-function renderIngredientList() {
-    const ingredientUl = document.querySelector('.ingredient-list');
+    render () {
+        const ingredientList = document.querySelector('.ingredient-list');
 
-    // Empty the ingredientList before adding any content to it.
-    ingredientUl.innerHTML = '';
+        // Empty the ingredientList before adding any content to it.
+        ingredientList.innerHTML = '';
 
-    ingredients.forEach(ingredient => {
-        const ingredientDiv = renderIngredientCard(ingredient);
-        ingredientUl.append(ingredientDiv)
-    })
+        ingredients.forEach(ingredient => {
+            const ingredientDiv = renderIngredientCard(ingredient);
+            ingredientList.append(ingredientDiv)
+        })
+    }
 }
 
 // Creates a DIV to display a single ingredient
